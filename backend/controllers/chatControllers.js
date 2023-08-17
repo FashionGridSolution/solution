5e80f2bcebbd1325426b6203af177892a5bc047d
DEBASMITROY2002 <ddruk2018@gmail.com>
2023-08-17 16:18:18 +0530

    flask_added

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -2,15 +2,15 @@ const http = require("http");
 const express = require("express");
 const socketIo = require("socket.io");
 const cors = require("cors"); // Import the cors middleware
-
+const axios=require('axios')
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
-    cors: {
-        origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
-      methods: ["GET", "POST"]
-    }
-  });
+  cors: {
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    methods: ["GET", "POST"]
+  }
+});
 
 // Use cors middleware to allow requests from any origin
 app.use(cors());
@@ -20,11 +20,26 @@ app.use(cors());
 app.use(cors());
 
 io.on("connection", (socket) => {
-  socket.on("message", (data) => {
+  socket.on("message", async (data) => {
     // Process the message, e.g., send a reply back
+    const msg = data.message;
     const reply = `Received message from User ${data.userId}: ${data.message}`;
-    console.log(data.message);
-    io.to(socket.id).emit("reply", { reply });
+    console.log(msg);
+    
+    //send the reqForModel to post to port 5000
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/getans", {
+        text: msg
+      });
+
+      const capitalizedMessage = response.data.result;
+      console.log("Capitalized message:", capitalizedMessage);
+      
+      io.to(socket.id).emit("reply", { reply: capitalizedMessage });
+    } catch (error) {
+      console.error("Error sending POST request:", error);
+      io.to(socket.id).emit("reply", { reply: "Error occurred while processing." });
+    }
   });
 });
 
---
+++ flask_server/main.py
@@ -0,0 +1,69 @@
+from langchain import OpenAI
+from langchain.chains import LLMChain, ConversationChain
+from langchain.chains.conversation.memory import (ConversationBufferMemory,
+                                                  ConversationSummaryMemory,
+                                                  ConversationBufferWindowMemory,
+                                                  ConversationKGMemory)
+from langchain.callbacks import get_openai_callback
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+import openai
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+import torch
+from langchain.llms import OpenAI
+from flask import Flask, request, jsonify
+from flask import Flask
+from flask_cors import CORS
+
+class ChatModel:
+    def __init__(self):
+        self.OPENAI_API_KEY = "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" #getpass()
+        self.llm = OpenAI(
+            temperature=0,
+            openai_api_key=self.OPENAI_API_KEY,
+            model_name='text-davinci-003'  # can be used with llms like 'gpt-3.5-turbo'
+        )
+
+        self.template = """
+        Provide fashion outfit recommendations for different occasions based on user queries.
+        Respond with appropriate outfit suggestions that suit the specified event or scenario.
+        Ensure the recommendations are stylish, suitable for the occasion, and consider factors such as weather, formality, and personal style. Always recommend 1 topwear, 1 bottomwear, 1 footwear and 1 accessory to go along with
+        Question: {question}
+        Answer:
+        """
+
+        self.prompt = PromptTemplate(
+            input_variables=["question"], template=self.template
+        )
+
+        self.conversation_sum = LLMChain(
+            llm=self.llm,
+            memory=ConversationSummaryMemory(llm=self.llm), prompt = self.prompt
+        )
+
+    def __call__(self, question):
+        return self.conversation_sum.run(question)
+
+
+
+app = Flask(__name__)
+CORS(app)
+chat_model = ChatModel()
+
+@app.route('/getans', methods=['POST'])
+def capitalize_text():
+    try:
+        data = request.json
+        input_text = data['text']
+        answer = chat_model(input_text)
+        # answer = input_text.upper()
+        urls = []
+        return jsonify({'result': answer, 'urls':urls}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+
+if __name__=="__main__":
+    print("Starting Flask Server")
+    app.run(debug=True, port=5000)
